6ea9dbc42f40d9d860d563044462e81301b94e8e
Sazz <stolensoul32@gmail.com>
2020-04-11 15:52:13 -0300

    Test

--- status.js
+++ status.js
@@ -5,7 +5,7 @@ let servers = require('./servers.json')
 
 async function poll () {
   status = await Promise.all(servers.map(async ({url, name}) => {
-    let response = await axios.get({url}).catch(e => e)
+    let response = await axios.get(url).catch(e => e)
     let online = (/* response.data.message === "OK" ? response.status : false */ false)
     console.log(response)
     return {url, name, online}
