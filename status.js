e3ae886b2173fa73af8f2ba23884b3f17e9e450b
Sazz <stolensoul32@gmail.com>
2020-04-13 15:03:10 -0300

    Last Incident

--- status.js
+++ status.js
@@ -3,6 +3,10 @@ const axios = require('axios')
 let status = []
 let servers = require('./servers.json')
 let online = false
+let existLast = false
+let last = []
+let date = Date()
+let time = date.toString()
 
 async function poll () {
   status = await Promise.all(servers.map(async ({url, name, type}) => {
@@ -10,9 +14,17 @@ async function poll () {
     switch(type) {
       case "api":
         online = (response.data.message === "OK" || response.statusText === "OK" ? true : false)
+        if(online === false) {
+          existLast = true
+          last = {name, type, time}
+        }
       break;
       case "website":
         online = (response.status === 200 ? true : false)
+        if(online === false) {
+          existLast === true
+          last = {name, type, time}
+        }
       break;
       case "star":
         online = true
@@ -31,4 +43,11 @@ function getStatus () {
   return status
 }
 
-module.exports = {start, getStatus}
+async function getIncident () {
+  if(!existLast) {
+    return { existLast }
+  }
+  return last
+}
+
+module.exports = {start, getStatus, getIncident}
